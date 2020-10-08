b0fb5a759ef2b4b11aa5d693ffc0191fbfb0bcd8
thaerachman <71365565+thaerachman@users.noreply.github.com>
2020-10-08 19:02:58 +0700

    Add files via upload

---
+++ tugas_6_1.php
@@ -0,0 +1,13 @@
+<?php
+$habib = 21;
+$aji=$habib + 4;
+$bintan=$aji + 3;
+
+echo "=============================== <br>";
+echo "Usia Habib = $habib tahun <br>";
+echo "Usia Aji = $aji tahun <br>";
+echo "Usia Bintan = $bintan tahun <br>";
+echo "=============================== <br>";
+
+
+?>
